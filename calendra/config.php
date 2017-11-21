87e2f63883f869f8245358455c4997de41c83a1e
bachiary <33873718+bachiary@users.noreply.github.com>
2017-11-21 18:20:41 +0100

    Add files via upload

---
+++ calendra/config.php
@@ -0,0 +1,17 @@
+;<?php exit; ?>
+;
+; Avactis configuration file
+;
+
+[DB]
+DB_SERVER = "localhost"
+DB_USER = "benjagol"
+DB_PASSWORD = "Montpel#2015"
+DB_NAME = "benjaminachiary_avactis"
+DB_TABLE_PREFIX = "asc_"
+DB_PCONNECT = "false"
+
+[SERVERINFO]
+HTTP_URL = "http://benjaminachiary.com/"
+COOKIEDOMAIN = "benjaminachiary.com"
+COOKIEPATH = "/"
